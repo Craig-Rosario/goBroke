38a3a07277a7b34209d5859480ac53fdaf10118c
Craig Rosario <craig.rosario04@gmail.com>
2025-04-08 14:36:03 +0530

    Added add and edit forms

--- budget_management/Expenses/script.js
+++ budget_management/Expenses/script.js
@@ -5,3 +5,26 @@ function toggleSideBar() {
     sidebar.classList.toggle('collapsed');
     button.classList.toggle('collapsed');
 }
+
+
+function openForm() {
+    document.getElementById("addForm").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("addForm").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+}
+
+
+function openEditForm() {
+    document.getElementById("editForm").style.display = "block";
+    document.getElementById("editOverlay").style.display = "block";
+}
+
+function closeEditForm() {
+    document.getElementById("editForm").style.display = "none";
+    document.getElementById("editOverlay").style.display = "none";
+}
+
--- budget_management/Income/script.js
+++ budget_management/Income/script.js
@@ -5,3 +5,23 @@ function toggleSideBar() {
     sidebar.classList.toggle('collapsed');
     button.classList.toggle('collapsed');
 }
+
+function openForm() {
+    document.querySelector('.addIncomeForm').style.display = "block";
+    document.querySelector('.incomeOverlay').style.display = "block";
+}
+
+function closeForm() {
+    document.querySelector('.addIncomeForm').style.display = "none";
+    document.querySelector('.incomeOverlay').style.display = "none";
+}
+
+function openEditForm() {
+    document.querySelector('.editIncomeForm').style.display = "block";
+    document.getElementById('editOverlay').style.display = "block";
+}
+
+function closeEditForm() {
+    document.querySelector('.editIncomeForm').style.display = "none";
+    document.getElementById('editOverlay').style.display = "none";
+}
