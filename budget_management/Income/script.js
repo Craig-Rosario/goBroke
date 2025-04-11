facaa0eb204f8e98bc0dea629892fabd8b7b0436
Craig Rosario <craig.rosario04@gmail.com>
2025-04-11 23:31:02 +0530

    Added reminders form and update linking

--- budget_management/Income/script.js
+++ budget_management/Income/script.js
@@ -6,22 +6,22 @@ function toggleSideBar() {
     button.classList.toggle('collapsed');
 }
 
-function openForm() {
-    document.querySelector('.addIncomeForm').style.display = "block";
-    document.querySelector('.incomeOverlay').style.display = "block";
+function openReminderForm() {
+    document.querySelector('.addReminderForm').style.display = "block";
+    document.querySelector('.reminderOverlay').style.display = "block";
 }
 
-function closeForm() {
-    document.querySelector('.addIncomeForm').style.display = "none";
-    document.querySelector('.incomeOverlay').style.display = "none";
+function closeReminderForm() {
+    document.querySelector('.addReminderForm').style.display = "none";
+    document.querySelector('.reminderOverlay').style.display = "none";
 }
 
-function openEditForm() {
-    document.querySelector('.editIncomeForm').style.display = "block";
-    document.getElementById('editOverlay').style.display = "block";
+function openEditReminderForm() {
+    document.querySelector('.editReminderForm').style.display = "block";
+    document.querySelector('.editReminderOverlay').style.display = "block";
 }
 
-function closeEditForm() {
-    document.querySelector('.editIncomeForm').style.display = "none";
-    document.getElementById('editOverlay').style.display = "none";
+function closeEditReminderForm() {
+    document.querySelector('.editReminderForm').style.display = "none";
+    document.querySelector('.editReminderOverlay').style.display = "none";
 }
--- budget_management/Reminders/script.js
+++ budget_management/Reminders/script.js
@@ -5,3 +5,23 @@ function toggleSideBar() {
     sidebar.classList.toggle('collapsed');
     button.classList.toggle('collapsed');
 }
+
+function openReminderForm() {
+    document.querySelector('.addReminderForm').style.display = "block";
+    document.querySelector('.reminderOverlay').style.display = "block";
+}
+
+function closeReminderForm() {
+    document.querySelector('.addReminderForm').style.display = "none";
+    document.querySelector('.reminderOverlay').style.display = "none";
+}
+
+function openEditReminderForm() {
+    document.querySelector('.editReminderForm').style.display = "block";
+    document.querySelector('.editReminderOverlay').style.display = "block";
+}
+
+function closeEditReminderForm() {
+    document.querySelector('.editReminderForm').style.display = "none";
+    document.querySelector('.editReminderOverlay').style.display = "none";
+}
